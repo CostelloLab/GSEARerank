69ce7dc5a5c8c9f3bc2c46df881e7452ff618770
tuh8888 <pielkekid@gmail.com>
2017-07-23 20:26:24 +0100

    Added calculation of average rank from distribution files

---
+++ gsearerank/src/main/java/edu/ucdenver/anschutz/cpbs/costello/App.java
@@ -0,0 +1,13 @@
+package edu.ucdenver.anschutz.cpbs.costello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
